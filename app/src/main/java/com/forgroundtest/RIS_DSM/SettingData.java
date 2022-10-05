484f5a94c223d1ba9bd6b1dd9463b60e844e5dc0
kingOfSchottland <46346267+kingOfSchottland@users.noreply.github.com>
2022-10-05 16:35:11 +0900

    🎨 add NBackTestUI / add required Adapter, nbackModel

---
+++ app/src/main/java/com/forgroundtest/RIS_DSM/SettingData.java
@@ -0,0 +1,19 @@
+package com.forgroundtest.RIS_DSM;
+
+public class SettingData {
+    private int icon;
+    private String menu;
+
+    public SettingData(int icon, String menu) {
+        this.icon = icon;
+        this.menu = menu;
+    }
+
+    public int getIcon() {
+        return icon;
+    }
+
+    public String getMenu() {
+        return menu;
+    }
+}
